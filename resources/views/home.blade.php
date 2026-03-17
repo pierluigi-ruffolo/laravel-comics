816f800324a0187fa92ee4eb522759dd26e01dc6
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2026-03-17 16:00:44 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends ('layouts.master')
+
+@section ('title')
+
+home
+@endsection
+
+
+
+
+@section ('content')
+<h1>Home</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,3 @@
+<div class="nav-bar">
+    <p>Nav bar</p>
+</div>
