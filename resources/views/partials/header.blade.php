f79e37a087f796d152ee9e1239f1076406c9a190
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2026-03-17 17:35:37 +0100

    feat: add functional header with logo and navigation links - Implemented navbar in header partial - Added dynamic routing using route names - Styled with SASS and Bootstrap classes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,19 @@
-<div class="nav-bar">
-    <p>Nav bar</p>
-</div>
+<nav class="navbar navbar-expand-lg bg-white shadow-sm">
+    <div class="container d-flex justify-content-between align-items-center">
+        <a class="navbar-brand" href="{{ route('home') }}">
+            <img class="img-logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo">
+        </a>
+
+        <ul class="navbar-nav d-flex flex-row gap-4 mb-0">
+            <li class="nav-item">
+                <a class="nav-link text-uppercase fw-bold" href="{{ route('home') }}">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-uppercase fw-bold" href="{{ route('comics') }}">Comics</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-uppercase fw-bold" href="{{ route('about') }}">About</a>
+            </li>
+        </ul>
+    </div>
+</nav>
